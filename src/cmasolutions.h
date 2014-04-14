2972b2f0c00dbfe3f27aa0daa719175bc19b31e6
Emmanuel Benazera <beniz@droidnik.fr>
2014-04-14 13:29:47 +0200

    full code comment of CMASolutions class

--- src/cmasolutions.h
+++ src/cmasolutions.h
@@ -10,29 +10,66 @@
 
 namespace libcmaes
 {
+  /**
+   * \brief Holder of the set of evolving solutions from running an instance
+   *        of CMA-ES.
+   */
   class CMASolutions
   {
   public:
+    /**
+     * \brief dummy constructor.
+     * DO NOT USE.
+     */
     CMASolutions() {};
+
+    /**
+     * \brief initializes solutions from stochastic optimization parameters.
+     * @param p parameters
+     */
     CMASolutions(Parameters &p);
+    
     ~CMASolutions();
 
+    /**
+     * \brief sorts the current internal set of solution candidates.
+     */
     void sort_candidates()
     {
       std::sort(_candidates.begin(),_candidates.end(),
 		[](Candidate const &c1, Candidate const &c2){return c1._fvalue < c2._fvalue;});
     }
 
+    /**
+     * \brief updates the history of best candidates, as well as other meaningful
+     *        values, typically used in termination criteria.
+     * @see CMAStopCriteria
+     */
     void update_best_candidates();
 
+    /**
+     * \brief updates reference eigenvalue and eigenvectors, for use in 
+     *        termination criteria.
+     * @see CMAStopCriteria
+     */
     void update_eigenv(const dVec &eigenvalues,
 		       const dMat &eigenvectors);
-    
+
+    /**
+     * \brief returns current best solution candidate.
+     *        NOTE: candidates MUST be sorted
+     * @return currentbest candidate
+     * @see CMASolutions::sort_candidates
+     */
     Candidate best_candidate() const
     {
       return _best_candidates_hist.back();
     }
-    
+
+    /**
+     * \brief number of candidate solutions.
+     * @return current number of solution candidates.
+     */
     int size() const
     {
       return _candidates.size();
@@ -41,12 +78,12 @@ namespace libcmaes
     dMat _cov; /**< covariance matrix. */
     dMat _csqinv; /** inverse root square of covariance matrix. */
     dVec _xmean; /**< distribution mean. */
-    dVec _psigma;
-    dVec _pc;
+    dVec _psigma; /**< cummulation for sigma. */
+    dVec _pc; /**< cumulation for covariance. */
     short _hsig; /**< 0 or 1. */
-    double _sigma;
-    std::vector<Candidate> _candidates;
-    std::vector<Candidate> _best_candidates_hist;
+    double _sigma; /**< step size. */
+    std::vector<Candidate> _candidates; /**< current set of candidate solutions. */
+    std::vector<Candidate> _best_candidates_hist; /**< history of best candidate solutions. */
 
     double _max_eigenv; /**< max eigenvalue, for termination criteria. */
     double _min_eigenv; /**< min eigenvalue, for termination criteria. */
@@ -62,8 +99,8 @@ namespace libcmaes
     bool _updated_eigen; /**< last update is not lazy. */
 
     // status of the run.
-    int _run_status;
-    int _elapsed_time;
+    int _run_status; /**< current status of the stochastic optimization (e.g. running, or stopped under termination criteria). */
+    int _elapsed_time; /**< final elapsed time of stochastic optimization. */
   };
 
 }
