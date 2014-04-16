2e0f0595728058b6e3d4b24a476a566d9bbe9e29
Emmanuel Benazera <beniz@droidnik.fr>
2014-04-16 15:42:01 +0200

    added LGPLv3 license

--- src/esostrategy.cc
+++ src/esostrategy.cc
@@ -1,7 +1,26 @@
+/**
+ * CMA-ES, Covariance Matrix Evolution Strategy
+ * Copyright (c) 2014 INRIA
+ * Author: Emmanuel Benazera <emmanuel.benazera@lri.fr>
+ *
+ * This file is part of libcmaes.
+ *
+ * libcmaes is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libcmaes is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libcmaes.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "esostrategy.h"
-#include "cmaparameters.h" // in order to pre-instanciated template into library.
-//#include "acmaparameters.h"
+#include "cmaparameters.h" // in order to pre-instanciate template into library.
 #include "cmasolutions.h"
 #include "cmastopcriteria.h"
 #include <iostream>
