8d0b2a26d1b51a2a513dc50ac79351d10023ac4a
Emmanuel Benazera <emmanuel.benazera@lri.fr>
2014-05-21 15:52:49 +0200

    in test_functions, default lambda is set automatically

--- tests/test-functions.cc
+++ tests/test-functions.cc
@@ -337,7 +337,7 @@ void printAvailFuncs()
 // command line options.
 DEFINE_string(fname,"fsphere","name of the function to optimize");
 DEFINE_int32(dim,2,"problem dimension");
-DEFINE_int32(lambda,10,"number of offsprings");
+DEFINE_int32(lambda,-1,"number of offsprings");
 DEFINE_int32(max_iter,-1,"maximum number of iteration (-1 for unlimited)");
 DEFINE_int32(max_fevals,-1,"maximum budget as number of function evaluations (-1 for unlimited)");
 DEFINE_bool(list,false,"returns a list of available functions");
