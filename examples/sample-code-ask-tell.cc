bd2f09845355ee21ad16191ea1872fc877d70d46
Emmanuel Benazera <emmanuel.benazera@lri.fr>
2014-06-05 15:45:15 +0200

    added sample code for demonstrating ask-tell interface / API, ref #45

---
+++ examples/sample-code-ask-tell.cc
@@ -0,0 +1,52 @@
+/**
+ * CMA-ES, Covariance Matrix Adaptation Evolution Strategy
+ * Copyright (c) 2014 Inria
+ * Author: Emmanuel Benazera <emmanuel.benazera@lri.fr>
+ *
+ * This file is part of libcmaes.
+ *
+ * libcmaes is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libcmaes is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libcmaes.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "cmaes.h"
+#include <iostream>
+
+using namespace libcmaes;
+
+FitFunc fsphere = [](const double *x, const int N)
+{
+  double val = 0.0;
+  for (int i=0;i<N;i++)
+    val += x[i]*x[i];
+  return val;
+};
+
+int main(int argc, char *argv[])
+{
+  int dim = 10; // problem dimensions.
+  std::vector<double> x0(dim,10.0);
+  double sigma = 0.1;
+
+  CMAParameters<> cmaparams(dim,&x0.front(),sigma);
+  ESOptimizer<CMAStrategy<CovarianceUpdate>,CMAParameters<>> optim(fsphere,cmaparams);
+
+  while(!optim.stop())
+    {
+      dMat candidates = optim.ask();
+      optim.eval(candidates);
+      optim.tell();
+      optim._niter++;
+    }
+  std::cout << optim._solutions << std::endl;
+}
