9c249213d3b5a6a9481d023099f5dd1e95015238
Emmanuel Benazera <emmanuel.benazera@lri.fr>
2014-06-13 01:03:09 +0200

    fixed logging of data for sep cmaes

--- src/cmastrategy.cc
+++ src/cmastrategy.cc
@@ -204,7 +204,9 @@ namespace libcmaes
     _fplotstream << fabs(eostrat<TGenoPheno>::_solutions._best_candidates_hist.back()._fvalue) << sep
 		 << eostrat<TGenoPheno>::_nevals << sep << eostrat<TGenoPheno>::_solutions._sigma << sep << sqrt(eostrat<TGenoPheno>::_solutions._max_eigenv/eostrat<TGenoPheno>::_solutions._min_eigenv) << sep;
     _fplotstream << eostrat<TGenoPheno>::_solutions._leigenvalues.transpose() << sep;
-    _fplotstream << eostrat<TGenoPheno>::_solutions._cov.sqrt().diagonal().transpose() << sep; // max deviation in all main axes
+    if (!eostrat<TGenoPheno>::_parameters._sep)
+      _fplotstream << eostrat<TGenoPheno>::_solutions._cov.sqrt().diagonal().transpose() << sep; // max deviation in all main axes
+    else _fplotstream << eostrat<TGenoPheno>::_solutions._sepcov.cwiseSqrt().transpose() << sep;
     _fplotstream << eostrat<TGenoPheno>::_solutions._xmean.transpose();
     _fplotstream << sep << eostrat<TGenoPheno>::_solutions._elapsed_last_iter;
 #ifdef HAVE_DEBUG
