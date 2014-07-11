0534ccc6bd3f4b9ad2a110bee3cdf31dd88a7586
Emmanuel Benazera <emmanuel.benazera@lri.fr>
2014-07-11 09:14:59 +0200

    lowered the numerical gradient overall budget to dim + 1

--- src/esostrategy.cc
+++ src/esostrategy.cc
@@ -95,16 +95,15 @@ namespace libcmaes
       return _gfunc(x.data(),_parameters._dim);
     dVec vgradf(_parameters._dim);
     dVec epsilon = 1e-8 * (dVec::Constant(_parameters._dim,1.0) + x.cwiseAbs());
+    double fx = _func(x.data(),_parameters._dim);
     for (int i=0;i<_parameters._dim;i++)
       {
 	dVec ei1 = x;
 	ei1(i,0) += epsilon(i);
-	dVec ei2 = x;
-	ei2(i,0) -= epsilon(i);
-	double gradi = (_func(ei1.data(),_parameters._dim) - _func(ei2.data(),_parameters._dim))/(2.0*epsilon(i));
+	double gradi = (_func(ei1.data(),_parameters._dim) - fx)/epsilon(i);
 	vgradf(i,0) = gradi;
       }
-    update_fevals(2*_parameters._dim); // numerical gradient increases the budget.
+    update_fevals(_parameters._dim+1); // numerical gradient increases the budget.
     return vgradf;
   }
 
