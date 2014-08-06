32647b8bb8cd7a8cd720015fb6f00cb31050a11b
Emmanuel Benazera <emmanuel.benazera@lri.fr>
2014-08-06 16:52:12 +0200

    added new CMAParameters constructor, straight from vector of double

--- src/cmaparameters.h
+++ src/cmaparameters.h
@@ -53,7 +53,23 @@ namespace libcmaes
 		const int &lambda=-1,
 		const uint64_t &seed=0,
 		const TGenoPheno &gp=GenoPheno<NoBoundStrategy>());
-    ~CMAParameters();
+
+  /**
+   * \brief Constructor.
+   * @param x0 initial search point as vector of problem dimension
+   * @param sigma initial distribution step size (positive, otherwise automatically set)
+   * @param lambda number of offsprings sampled at each step
+   * @param seed initial random seed, useful for reproducing results (if unspecified, automatically generated from current time)
+   * @param gp genotype / phenotype object
+   * @param sep whether to use sep-CMA-ES, using diagonal covariance matrix (modifies covariance default learning rate)
+   */
+  CMAParameters(const std::vector<double> &x0,
+		const double &sigma,
+		const int &lambda=-1,
+		const uint64_t &seed=0,
+		const TGenoPheno &gp=GenoPheno<NoBoundStrategy>());
+
+  ~CMAParameters();
 
     /**
      * \brief initialize required parameters based on dim, lambda, x0 and sigma.
