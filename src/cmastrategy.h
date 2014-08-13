510bee3f3aab394392eeebe56b385e89c41bb9bd
Emmanuel Benazera <emmanuel.benazera@lri.fr>
2014-08-13 17:59:41 +0200

    added dummy constructors for ESOptimizer and CMAStrategy as convenience for declaring object in external classes + used pointer for fplotstream as streams are not copyable
    Conflicts:
    	src/cmastrategy.cc
    	src/cmastrategy.h

--- src/cmastrategy.h
+++ src/cmastrategy.h
@@ -90,7 +90,7 @@ namespace libcmaes
     private:
       EigenMultivariateNormal<double> _esolver;  /**< multivariate normal distribution sampler, and eigendecomposition solver. */
       CMAStopCriteria<TGenoPheno> _stopcriteria; /**< holds the set of termination criteria, see reference paper. */
-      std::ofstream _fplotstream; /**< plotting file stream, not in parameters because of copy-constructor hell. */
+      std::ofstream *_fplotstream = nullptr; /**< plotting file stream, not in parameters because of copy-constructor hell. */
 
     public:
     static ProgressFunc<CMAParameters<TGenoPheno>,CMASolutions> _defaultPFunc; /**< the default progress function. */
