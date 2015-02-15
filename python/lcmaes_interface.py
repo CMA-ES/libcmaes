d8f166ce0e79c08dd80243b8b5f8f46f7affbf56
nikohansen <nikohansen@users.noreply.github.com>
2015-02-15 15:16:43 +0100

    unicode bug fix

--- python/lcmaes_interface.py
+++ python/lcmaes_interface.py
@@ -1,36 +1,39 @@
-"""usage (yet versatile): 
+"""usage (yet versatile):
 
-    import lcmaes_interface as lci 
-    
+    import lcmaes_interface as lci
+
     # setup input parameters
     myfun = lambda x: sum([xi**2 for xi in x])  # myfun accepts a list of numbers as input
     x0 = [2.1] * 10
     sigma0 = 0.1
-    
+
     # run optimization via lci
-    res = lci.lcmaes.pcmaes(lci.to_fitfunc(myfun), lci.to_params(x0, sigma0))
+    res = lci.lcmaes.pcmaes(lci.to_fitfunc(myfun),
+                            lci.to_params(x0, sigma0,
+                                str_algo=b'aipop', # b=bytes, unicode fails
+                                restarts=2))
     lci.plot()  # plot from file set in lci.to_params
-    
-Details: for the time being `to_params` is based on `lcmaes.make_simple_parameters`, 
-but that might change in future. 
+
+Details: for the time being `to_params` is based on `lcmaes.make_simple_parameters`,
+but that might change in future.
 
 """
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 import lcmaes
 import cma_multiplt as cmaplt
-fplot_current = 'lcmaes.dat'
+fplot_current = b'lcmaes.dat'
 
-def to_params(x0, sigma0, str_algo="acmaes", fplot=None, **kwargs):
+def to_params(x0, sigma0, str_algo=b'acmaes', fplot=None, **kwargs):
     """return parameter object instance for `lcmaes.pcmaes`.
-    
+
     Keys in `kwargs` must correspond to `name` in `set_name` attributes
-    of `lcmaes.CMAParameters`, e.g. ``ftarget=1e-7``. 
-    
-    Details: when `fplot is None` (default), the default output filename 
-    is used. 
+    of `lcmaes.CMAParameters`, e.g. ``ftarget=1e-7``.
+
+    Details: when `fplot is None` (default), the default output filename
+    is used.
     """
-    # TODO: add further parameters explicitly? 
+    # TODO: add further parameters explicitly?
     p = lcmaes.make_simple_parameters(x0, sigma0)
     p.set_str_algo(str_algo)
     if fplot and fplot != True:  # then fplot must be filename
@@ -48,7 +51,7 @@ def to_params(x0, sigma0, str_algo="acmaes", fplot=None, **kwargs):
 def to_fitfunc(f):
     """return function for lcmaes from callable `f`, where `f` accepts a list of numbers as input."""
     return lcmaes.fitfunc_pbf.from_callable(lambda x, n: f(x))
-    
+
 def plot(file=None):
     cmaplt.plot(file if file else fplot_current)
-    
+
