d4e2a4ac29776b1117810e0a6d586f057bdbdc9a
Emmanuel Benazera <emmanuel.benazera@lri.fr>
2015-02-16 18:45:19 +0100

    fixed plot window in simple.py, ref #122

--- python/simple.py
+++ python/simple.py
@@ -20,3 +20,10 @@ cmasols = lcmaes.pcmaes(lcmaes.fitfunc_pbf.from_callable(nfitfunc), p)
 
 # visualize results
 lcmaplt.plot(outfile)
+
+if __name__ == "__main__":
+    msg = '  --- press return to continue --- '
+    try: 
+        raw_input(msg) 
+    except NameError: 
+        input(msg)
